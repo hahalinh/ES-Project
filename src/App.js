91086b36bcf668c83088d5f5705835a82736b086
Huy <ngoviduchuy123@gmail.com>
2023-03-08 17:02:56 -0500

    functional open feedings

--- src/App.js
+++ src/App.js
@@ -7,48 +7,72 @@ import Provider from './components/Provider';
 import Recipient from './components/Recipient';
 import Timer from './components/Timer';
 import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 
 function App() {
-  const [feeding, setFeeding] = useState({
-    feedingID: 1,
-    nest: 2,
-    timeArrive: "15:30",
-    timeDepart: "15:31",
-    provider: "S",
-    recipent: "S",
-    preyItem: "K",
-    preySize: 1.75,
-    numberItems: 1
-  })
+  const initialFeeding = {
+    feedingID: uuid().slice(0, 8),
+    nest: 0,
+    timeArrive: "",
+    timeDepart: "",
+    provider: "",
+    recipent: "",
+    preyItem: "",
+    preySize: 0,
+    numberItems: 0
+  }
+  const [feeding, setFeeding] = useState(initialFeeding)
+
+  const [feedings, setFeedings] = useState([]);
 
   const setNest = (nest) => {
-    setFeeding({...feeding, nest: nest});
+    setFeeding({ ...feeding, nest: nest });
   }
-  
+
   const setRecipient = (recipent) => {
-    setFeeding({...feeding, recipent: recipent});
+    setFeeding({ ...feeding, recipent: recipent });
   }
 
   const setProvider = (provider) => {
-    setFeeding({...feeding, provider: provider});
+    setFeeding({ ...feeding, provider: provider });
   }
 
   const setPreyItem = (preyItem) => {
-    setFeeding({...feeding, preyItem: preyItem});
+    setFeeding({ ...feeding, preyItem: preyItem });
   }
 
   const setPreySize = (preySize) => {
-    setFeeding({...feeding, preySize: preySize});
+    setFeeding({ ...feeding, preySize: preySize });
   }
 
   const setTimeArrive = () => {
     const d = new Date();
-    setFeeding({...feeding, timeArrive: d.toTimeString().slice(0, 8), timeDepart: null})
+    setFeeding({ ...feeding, timeArrive: d.toTimeString().slice(0, 8), timeDepart: "" })
   }
 
   const setTimeDepart = () => {
     const d = new Date();
-    setFeeding({...feeding, timeDepart: d.toTimeString().slice(0, 8)})
+    setFeeding({ ...feeding, timeDepart: d.toTimeString().slice(0, 8) })
+  }
+
+  const handleNewFeeding = () => {
+    const exist = feedings.findIndex(item => item.feedingID === feeding.feedingID);
+
+    if (exist >= 0) {
+      let newFeeding = [...feedings];
+      newFeeding[exist] = feeding;
+      setFeedings(newFeeding);
+    }
+    else if (exist < 0) {
+      setFeedings([...feedings, feeding]);
+    }
+    setFeeding(initialFeeding);
+  }
+
+  const handleOpenFeeding = (e) => {
+    const feedingId = e.currentTarget.value;
+    const curr = feedings.find(item => item.feedingID === feedingId);
+    setFeeding(curr);
   }
 
   return (
@@ -58,6 +82,16 @@ function App() {
 
         <div>
           <p>Open Feedings:</p>
+          {
+            feedings.map((item, index) => {
+              return (
+                <input key={index} value={item.feedingID} type="button"
+                  onClick={(e) => handleOpenFeeding(e)}
+                />
+              )
+            })
+          }
+          <button onClick={handleNewFeeding}>New</button>
         </div>
       </div>
 
@@ -73,13 +107,13 @@ function App() {
       </div>
 
 
-      <div>{feeding.timeArrive}</div>
-      <div>{feeding.timeDepart}</div>
-      <div>{feeding.nest}</div>
-      <div>{feeding.provider}</div>
-      <div>{feeding.recipent}</div>
-      <div>{feeding.preyItem}</div>
-      <div>{feeding.preySize}</div>
+      <div>time arrive: {feeding.timeArrive}</div>
+      <div>time depart: {feeding.timeDepart}</div>
+      <div>nest: {feeding.nest}</div>
+      <div>provider: {feeding.provider}</div>
+      <div>recipient: {feeding.recipent}</div>
+      <div>prey item: {feeding.preyItem}</div>
+      <div>prey size: {feeding.preySize}</div>
     </div>
   );
 }
