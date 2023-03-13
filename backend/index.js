12718f1f670cade746b99b701775379f35d556fb
Huy <ngoviduchuy123@gmail.com>
2023-03-12 20:24:20 -0400

    add database

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const processData = require('./processData');
-
 const fileCSV = 'data.csv';
 const fileJSON = 'data.json';
+
+const mongoose = require('mongoose');
+const server = "mongodb+srv://nvduchuy04:ngoviduchuy123@esp.l9jrx9g.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(server,
+    { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.connection.on('error', err => console.log('Error connecting to DB', err))
+mongoose.connection.once('open', () => console.log('Connected successfully!'))
+
+const FeedingModel = mongoose.model('feeding', new mongoose.Schema({
+    feedingID: String,
+    nest: Number,
+    timeArrive: String,
+    timeDepart: String,
+    provider: String,
+    recipent: String,
+    preyItem: String,
+    preySize: Number,
+    numberItems: Number
+}))
 
 app.post('/', async (req, res) => {
     const data = req.body;
