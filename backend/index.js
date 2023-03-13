70da64a1f3bee09eb1a880b8776c9e1abc391989
Huy <ngoviduchuy123@gmail.com>
2023-03-12 20:45:33 -0400

    update database

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const fileCSV = 'data.csv';
 const fileJSON = 'data.json';
 
 const mongoose = require('mongoose');
-const server = "mongodb+srv://nvduchuy04:ngoviduchuy123@esp.l9jrx9g.mongodb.net/?retryWrites=true&w=majority";
+const server = "mongodb+srv://nvduchuy04:ngoviduchuy123@esp.l9jrx9g.mongodb.net/test?retryWrites=true&w=majority";
 
 mongoose.connect(server,
     { useNewUrlParser: true, useUnifiedTopology: true })
@@ -20,15 +20,19 @@ mongoose.connection.on('error', err => console.log('Error connecting to DB', err
 mongoose.connection.once('open', () => console.log('Connected successfully!'))
 
 const FeedingModel = mongoose.model('feeding', new mongoose.Schema({
-    feedingID: String,
-    nest: Number,
-    timeArrive: String,
-    timeDepart: String,
-    provider: String,
-    recipent: String,
-    preyItem: String,
-    preySize: Number,
-    numberItems: Number
+    feedingData: [
+        {
+            feedingID: String,
+            nest: String,
+            timeArrive: String,
+            timeDepart: String,
+            provider: String,
+            recipent: String,
+            preyItem: String,
+            preySize: String,
+            numberItems: Number
+        }
+    ]
 }))
 
 app.post('/', async (req, res) => {
@@ -37,9 +41,10 @@ app.post('/', async (req, res) => {
     const allData = await processData.readJSON(fileJSON);
     await processData.saveCSV(fileCSV, allData);
 
-    res.send({
-        msg: "Successfully save!"
-    });
+    const feeding = new FeedingModel({feedingData: data});
+    await feeding.save();
+
+    res.send(feeding);
 })
 
 app.get('/', async (req, res) => {
