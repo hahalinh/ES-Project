5fa598ab799e7c7de28f19449b058254ac5a7bbe
Duc Huy <51442563+1232145@users.noreply.github.com>
2023-03-27 12:05:08 -0400

    Update NumberItems.jsx

--- src/components/feeding/NumberItems.jsx
+++ src/components/feeding/NumberItems.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Button from '../Button'
 
-function NumberItems({setNumberItems}) {
+function NumberItems({setNumberItems, data}) {
     return (
         <div className="number-items">
-            <p>Number of Items</p>
-            <Button handleData={setNumberItems} value={1}/>
+            <p>Number of Items: {data}</p>
             <input placeholder='other' type='number' onChange={(e) => setNumberItems(e.currentTarget.value)}/>
         </div>
     )
