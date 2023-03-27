a78c0dc36f122a1c805ed11a0d11f05d6541b8e6
Duc Huy <51442563+1232145@users.noreply.github.com>
2023-03-27 12:05:46 -0400

    Update Provider.jsx

--- src/components/feeding/Provider.jsx
+++ src/components/feeding/Provider.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Button from '../Button';
 
-function Provider({ setProvider }) {
+function Provider({ setProvider, data }) {
     return (
         <div className="provider">
-            <p>Provider</p>
+            <p>Provider: {data}</p>
             <div className="provider-bt">
                 <Button handleData={setProvider} value="P1" />
                 <Button handleData={setProvider} value="P2" />
