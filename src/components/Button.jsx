a02fb4c4d70aab097a44fc9ab00070aa7ef1f4ca
Linh Dam <linhdam@dhcp-218-230.cc.gettysburg.edu>
2023-05-10 00:04:45 -0400

    Add color to selected buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,32 +1,3 @@
-
-
-// import React from 'react'
-
-// /**
-//  * Returns a button which handle input
-//  * @param {*} param0 
-//  * @returns 
-//  */
-// function Button({ handleData, value, type, className }) {
-//     if (value === "") {
-//         return (
-//             <input onClick={() => handleData("")}
-//             value="Clear" 
-//             type={type ? type : "button"} className={className}
-//         />
-//         )
-//     }
-
-//     return (
-//         <input 
-//             onClick={(e) => handleData(e.currentTarget.value)}
-//             value={value} type={type ? type : "button"} className={className}
-//         />
-//     )
-// }
-
-// export default Button
-
 import React from 'react';
 import '../App.css';
 
